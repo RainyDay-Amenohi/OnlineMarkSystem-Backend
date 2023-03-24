43378e42b892b2590643477e581405edeb7dd951
Rdy <1972245397@qq.com>
2023-03-25 00:49:51 +0800

    update

--- omsBackend/settings.py
+++ omsBackend/settings.py
@@ -149,6 +149,6 @@ REST_FRAMEWORK = {
 
 # JWT配置
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
--- questions/admin.py
+++ questions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from questions.models import ChoiceQuestion, SubjectiveQuestion
+from questions.models import ChoiceQuestion, SubjectiveQuestion, BlankQuestion
 
 # Register your models here.
 # admin.site.register(SingleChoice)
 admin.site.register(ChoiceQuestion)
 admin.site.register(SubjectiveQuestion)
+admin.site.register(BlankQuestion)

4ee9e408bc926d2a0250b3a60cd259653e627f26
Rdy <1972245397@qq.com>
2023-04-17 21:29:38 +0800

    fix: 修复了随机组卷功能的bug

--- exams/serializers.py
+++ exams/serializers.py
@@ -18,6 +18,7 @@ class ExamSerializer(serializers.ModelSerializer):
 class ExamQuestionSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='examquestion-detail')
     type_name = serializers.ReadOnlyField(source='get_type_display')
+    score = serializers.IntegerField(required=False, default=0)
     # 在反序列化的时候，接收一个主键值或者一个包含多个主键值的列表，然后根据queryset参数找到对应的模型对象
     exam = serializers.PrimaryKeyRelatedField(queryset=Exam.objects.all())
 
